a462fd609f9e3e4c2ad08a6653ce5acfabca7ebd
wh1zzRD <tim@tim-seleznev-f4d4887f9645.iserv.kkg.berlin>
2025-02-11 09:18:21 +0100

    Imporve simplification for negation operation.

---
+++ src/constants.rs
@@ -0,0 +1,6 @@
+pub const PI: f64 = 3.141592653589793;
+pub const E: f64 = 2.718281828459045;
+
+// alternative
+// pub const PI: f64 = std::f64::consts::PI;
+// pub const E: f64 = std::f64::consts::E;
--- src/simplify.rs
+++ src/simplify.rs
@@ -1,13 +1,15 @@
 use crate::differentiate::is_elementary_function;
+use crate::constants::E;
 use crate::parser::{BinaryOpKind, Expr};
 
-pub fn simplify(expr: Expr) -> Expr {
+pub fn simplify(expr: Expr, s: bool) -> Expr {
+    // s = simplify_elementary_function
     match expr {
         Expr::Number(_) => expr,
         Expr::Var(_) => expr,
         Expr::BinaryOp(op, left, right) => {
-            let left = simplify(*left);
-            let right = simplify(*right);
+            let left = simplify(*left, s);
+            let right = simplify(*right, s);
             match (op, left.clone(), right.clone()) {
                 (BinaryOpKind::Add, Expr::Number(a), Expr::Number(b)) => Expr::Number(a + b),
                 (BinaryOpKind::Sub, Expr::Number(a), Expr::Number(b)) => Expr::Number(a - b),
@@ -25,44 +27,70 @@ pub fn simplify(expr: Expr) -> Expr {
             }
         }
         Expr::Call(func, args) => {
-            let value = simplify_call(*func, &args);
+            let value = simplify_call(*func, &args, s);
             match value {
                 Expr::Call(func, args) => Expr::Call(func, args),
-                _ => simplify(value),
+                _ => simplify(value, s),
             }
         }
         Expr::UnaryOp(op, expr) => {
-            let expr = simplify(*expr);
+            let expr = simplify(*expr, s);
             match (op, expr.clone()) {
-                (crate::parser::UnaryOpKind::Neg, Expr::Number(0.0)) => Expr::Number(0.0),
+                (crate::parser::UnaryOpKind::Neg, Expr::Number(x)) => Expr::Number(-x),
+                (crate::parser::UnaryOpKind::Neg, Expr::BinaryOp(BinaryOpKind::Add, left, right)) => {
+                    Expr::BinaryOp(
+                        BinaryOpKind::Add,
+                        Box::new(simplify(Expr::UnaryOp(crate::parser::UnaryOpKind::Neg, left), s)),
+                        Box::new(simplify(Expr::UnaryOp(crate::parser::UnaryOpKind::Neg, right), s)),
+                    )
+                }
+                (crate::parser::UnaryOpKind::Neg, Expr::BinaryOp(BinaryOpKind::Sub, left, right)) => {
+                    Expr::BinaryOp(
+                        BinaryOpKind::Sub,
+                        Box::new(simplify(Expr::UnaryOp(crate::parser::UnaryOpKind::Neg, left), s)),
+                        right,
+                    )
+                }
+
                 _ => Expr::UnaryOp(op, Box::new(expr)),
             }
         }
     }
 }
 
-fn simplify_call(func: Expr, args: &Vec<Expr>) -> Expr {
-    let args: Vec<Expr> = args.iter().map(|arg| simplify(arg.clone())).collect();
+fn simplify_call(func: Expr, args: &Vec<Expr>, s: bool) -> Expr {
+    // s = simplify_elementary_function
+    let args: Vec<Expr> = args.iter().map(|arg| simplify(arg.clone(), s)).collect();
     match func.clone() {
         Expr::Var(name) => {
             if name == "id" {
                 assert!(args.len() == 1);
                 return args[0].clone()
             } else if is_elementary_function(&name){
+                if s {
+                    evaluate_elementary_function(&name);
+                }
                 return Expr::Call(Box::new(func.clone()), args.clone());
             }
             Expr::Var(name.clone())
         },
         Expr::Number(_) => func.clone(),
         Expr::Call(func, args) => {
-            simplify_call(*func, &args)
+            simplify_call(*func, &args, s)
         },
         Expr::UnaryOp(op, expr) => {
-            Expr::UnaryOp(op, Box::new(simplify_call(*expr, &args)))
+            Expr::UnaryOp(op, Box::new(simplify_call(*expr, &args, s)))
         },
         Expr::BinaryOp(op, left, right) => {
-            Expr::BinaryOp(op, Box::new(simplify_call(*left, &args)), Box::new(simplify_call(*right, &args)))
+            Expr::BinaryOp(op, Box::new(simplify_call(*left, &args, s)), Box::new(simplify_call(*right, &args, s)))
         },
         _ => Expr::Call(Box::new(func.clone()), args.clone()),
+    }
+}
+
+fn evaluate_elementary_function(name: &str) -> Expr {
+    // TODO
+    match name {
+        _ => panic!("Not implemented")
     }
 }
