bd675e655213cd79bbd46a3b96bdf13a80006c7e
wh1zzRD <tim@tim-seleznev-f4d4887f9645.iserv.kkg.berlin>
2025-02-13 09:19:54 +0100

    Add function to create values table.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use std::collections::HashMap;
 use crate::differentiate::differentiate;
-use crate::integrate::integrate;
-use crate::parser::Expr;
-use crate::plot::substitute_for_variable;
+use crate::plot::values_table;
 use crate::render::render_latex;
 use crate::scanner::Scanner;
 use crate::simplify::simplify;
@@ -13,15 +10,13 @@ mod differentiate;
 mod eval;
 mod render;
 mod simplify;
-mod integrate;
-mod evaluate;
 mod constants;
 mod plot;
 
 fn main() {
     // let source = "a * (b + c)";
     // let source = "5 * (x^2) + 2 * x + 7";
-    let source = "y * (x + 5)";
+    let source = "x ^ 2";
     let mut scanner = Scanner::new(source);
     let mut tokens = vec![];
     while let token = scanner.scan_token() {
@@ -33,12 +28,13 @@ fn main() {
     let mut parser = parser::Parser::new(tokens);
     let expression = parser.expression();
 
-    let mut variables = HashMap::new();
-    variables.insert("x".to_string(), Expr::Number(5.0));
-    variables.insert("y".to_string(), Expr::Number(3.5));
 
     println!("expression: {:?}", expression);
-    println!("expression: {:?}", substitute_for_variable(expression, &variables));
 
+    let (x_values, y_values) = values_table(expression);
 
+    // Print x-values and corresponding y-values
+    for (x, y) in x_values.iter().zip(y_values.iter()) {
+        println!("{:?}: {:?}", x, y);
+    }
 }
--- src/plot.rs
+++ src/plot.rs
@@ -2,7 +2,7 @@ use crate::simplify::simplify;
 use crate::parser::{BinaryOpKind, Expr};
 use std::collections::HashMap;
 
-pub fn substitute_for_variable(expr: Expr, variables: &HashMap<String, Expr>) -> Expr {
+fn substitute_for_variable(expr: Expr, variables: &HashMap<String, Expr>) -> Expr {
     match expr {
         Expr::Var(var) => {
             if let Some(value) = variables.get(&var) {
@@ -20,4 +20,27 @@ pub fn substitute_for_variable(expr: Expr, variables: &HashMap<String, Expr>) ->
         }
         _ => expr,
     }
+}
+
+pub fn values_table(expr: Expr) -> (Vec<Expr>, Vec<Expr>) {
+    // TODO add iteration over the expression to find all the variables
+    // for now the function only works for the variable x
+
+    let mut x_values: Vec<Expr> = Vec::new();
+    let mut y_values: Vec<Expr> = Vec::new();
+    // i am lowkey sorry about this code
+    let mut i = -5.0;
+    while i <= 5.0 {
+        let mut variables = HashMap::new();
+        variables.insert("x".to_string(), Expr::Number(i));
+
+        let simplified_expr = simplify(substitute_for_variable(expr.clone(), &variables), false);
+
+        x_values.push(Expr::Number(i));
+        y_values.push(simplified_expr);
+
+        i += 0.5;
+    }
+
+    (x_values, y_values)
 }
