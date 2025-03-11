18539f171b18b9918205e19c6be29ec765e65476
wh1zzRD <seleznev_tim@outlook.com>
2025-03-11 14:21:00 +0100

    Add basic integration.

--- src/integrate.rs
+++ src/integrate.rs
@@ -1,21 +1,65 @@
-extern crate dotenv;
+// extern crate dotenv;
+//
+// use crate::parser::{BinaryOpKind, Expr};
+// use crate::simplify::simplify;
+// use crate::substitute::substitute;
+//
+// pub fn integrate(expr: Expr, var: String, lower: f64, upper: f64) -> Result<Expr, String> {
+//     let mut result = 0.0;
+//     let mut x = lower;
+//     let dx = 0.0001;
+//     while x < upper {
+//         let y = simplify(substitute(expr.clone(), var.clone(), Expr::Number(x)), false);
+//         if let Expr::Number(value) = y {
+//             result += value * dx;
+//         } else {
+//             return Err("Integration failed: expression did not simplify to a number".to_string());
+//         }
+//         x += dx;
+//     }
+//     Ok(Expr::Number(result))
+// }
 
-use crate::parser::Expr;
-use crate::simplify::simplify;
-use crate::substitute::substitute;
+use crate::parser::{Expr, BinaryOpKind};
 
-pub fn integrate(expr: Expr, var: String, lower: f64, upper: f64) -> Result<Expr, String> {
-    let mut result = 0.0;
-    let mut x = lower;
-    let dx = 0.0001;
-    while x < upper {
-        let y = simplify(substitute(expr.clone(), var.clone(), Expr::Number(x)), false);
-        if let Expr::Number(value) = y {
-            result += value * dx;
-        } else {
-            return Err("Integration failed: expression did not simplify to a number".to_string());
+pub fn integrate_polynomial(expr: Expr, var: String) -> Expr {
+    match expr {
+        Expr::Number(a) => Expr::BinaryOp(BinaryOpKind::Mul, Box::new(Expr::Number(a)), Box::new(Expr::Var(var))),
+        Expr::Var(v) => {
+            if v == var {
+                Expr::Number(1.0)
+            } else {
+                Expr::Number(0.0)
+            }
         }
-        x += dx;
+        Expr::BinaryOp(op, left, right) => integrate_binary_op(op, *left, *right, var.clone()),
+        _ => panic!("Not implemented"),
     }
-    Ok(Expr::Number(result))
+
+}
+
+fn integrate_binary_op(op: BinaryOpKind, left: Expr, right: Expr, var: String) -> Expr {
+    match (op, left, right) {
+        (BinaryOpKind::Mul, Expr::Number(a), Expr::BinaryOp(BinaryOpKind::Pow, base, exponent)) => {
+            // Dereferencing Box to access inner values
+            if let (Expr::Var(x), Expr::Number(b)) = (*base, *exponent) {
+                Expr::BinaryOp(
+                    BinaryOpKind::Mul,
+                    Box::new(Expr::BinaryOp(
+                        BinaryOpKind::Div,
+                        Box::new(Expr::Number(a)),
+                        Box::new(Expr::Number(b + 1.0)),
+                    )),
+                    Box::new(Expr::BinaryOp(
+                        BinaryOpKind::Pow,
+                        Box::new(Expr::Var(x)),
+                        Box::new(Expr::Number(b + 1.0)),
+                    )),
+                )
+            } else {
+                panic!("Unsupported exponentiation expression in polynomial integration");
+            }
+        }
+        _ => panic!("Not implemented"),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use crate::differentiate::differentiate;
 use crate::render::{render_latex};
 use crate::scanner::Scanner;
 use crate::simplify::simplify;
-use crate::integrate::integrate;
+use crate::integrate::{integrate_polynomial};
 use crate::parser::Expr;
 
 mod scanner;
@@ -18,8 +18,8 @@ mod plot;
 
 fn main() {
     // let source = "a * (b + c)";
-    // let source = "(x^3) + 3 * (x^2) + 2";
-    let source = "cos(x)";
+    let source = "2 * (x^2)";
+    // let source = "cos(x)";
     // let source = "7 * (x^4) - 3 * (x^3) + 5 * (x^2) - 8 * x + 2";
     let mut scanner = Scanner::new(source);
     let mut tokens = vec![];
@@ -35,4 +35,7 @@ fn main() {
     println!("expression: {:?}", expression.clone());
     println!("expression: {:?}", simplify(expression.clone(), false));
     println!("{}", render_latex(simplify(differentiate(expression.clone(), "x".to_string()), false)));
+
+    println!("{}", render_latex(simplify(integrate_polynomial(expression.clone(), "x".to_string()), false)));
+
 }
