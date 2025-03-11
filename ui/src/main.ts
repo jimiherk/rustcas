b7f1b7745635e400a93c8ea0e94841edc1893b6b
Jimi <mail@jimi.berlin>
2025-03-11 23:57:08 +0100

    💬 Comment front-end code too

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,12 +1,14 @@
 import { differentiate_expression, integrate_expression, simplify_expression, plot_expression } from '../wasm/index';
 import TeXToSVG from "tex-to-svg";
 
+// Event-Listener für die Änderung der Operation
 document.querySelector('select#operation')?.addEventListener('change', () => {
     const operation = (document.querySelector('select#operation') as HTMLSelectElement).value;
     const varInput = document.querySelector('input#variable')!;
     const lowerInput = document.querySelector('input#lower')!;
     const upperInput = document.querySelector('input#upper')!;
 
+    // Sichtbarkeit der Eingabefelder basierend auf der ausgewählten Operation ändern
     switch (operation) {
         case 'Ableiten':
             varInput.classList.remove('hidden');
@@ -28,6 +30,7 @@ document.querySelector('select#operation')?.addEventListener('change', () => {
     }
 });
 
+// Event-Listener für das Absenden des Formulars
 document.querySelector('form#inputForm')?.addEventListener('submit', () => {
     const input = (document.querySelector('input#input') as HTMLInputElement).value;
     const output = document.querySelector('div#output')!;
@@ -39,6 +42,7 @@ document.querySelector('form#inputForm')?.addEventListener('submit', () => {
 
     let result: string;
     console.log(operation);
+    // Ergebnis basierend auf der ausgewählten Operation berechnen
     switch (operation) {
         case 'Ableiten':
             result = differentiate_expression(input, varInput.value);
@@ -53,21 +57,21 @@ document.querySelector('form#inputForm')?.addEventListener('submit', () => {
             throw new Error('Invalid operation');
     }
 
+    // Plot der Eingabe erzeugen
     let plot: Uint8Array | string = plot_expression(input);
-
     plot = URL.createObjectURL(new Blob([plot.buffer], { type: 'image/png' }));
 
     output.innerHTML = '';
 
+    // Bild des Plots hinzufügen
     let imageElement = document.createElement('img');
     imageElement.src = plot as string;
-
     output.appendChild(imageElement);
 
+    // Ergebnis anzeigen
     let outputElement = document.createElement('div');
     outputElement.id = 'results';
     outputElement.innerHTML = `<p>Deine Eingabe:</p> ${TeXToSVG(input)}`;
     outputElement.innerHTML += `<p>Ergebnis:</p> ${TeXToSVG(result)}`;
-
     output.appendChild(outputElement);
 });
