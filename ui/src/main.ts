d3179b95da458e687e09cfaf44c9687b3cb98aa2
Jimi <mail@jimi.berlin>
2025-03-11 14:05:49 +0100

    🖼️ Add TeX -> SVG Conversion

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,3 +1,26 @@
 import { differentiate_expression, integrate_expression, simplify_expression } from '../wasm/index';
+import TeXToSVG from "tex-to-svg";
 
-console.log(differentiate_expression('x^2', 'x'));
+document.querySelector('form#inputForm')?.addEventListener('submit', () => {
+    const input = (document.querySelector('input#input') as HTMLInputElement).value;
+    const output = document.querySelector('div#output')!;
+    const operation = (document.querySelector('select#operation') as HTMLSelectElement).value;
+
+    let result: string;
+    console.log(operation);
+    switch (operation) {
+        case 'Ableiten':
+            result = differentiate_expression(input, "x");
+            break;
+        case 'Integrieren':
+            result = integrate_expression(input, "x", 0, 1);
+            break;
+        case 'Vereinfachen':
+            result = simplify_expression(input);
+            break;
+        default:
+            throw new Error('Invalid operation');
+    }
+
+    output.innerHTML = TeXToSVG(result);
+});
